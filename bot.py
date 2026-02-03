a5975386954610b4b1e705d6725ea5f0443b4c59
Razanvet <120029950+Razanvet@users.noreply.github.com>
2026-02-03 20:45:49 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Bot
+
+# ===== НАСТРОЙКИ =====
+BOT_TOKEN = "8552290162:AAGHM0pmC6BuCjE4NlTqG0N3pIGNZ4r4lCc"
+CHAT_ID = "1200659505"
+
+bot = Bot(token=BOT_TOKEN)
+
+async def send_alert(message: str):
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text=message
+    )
+
