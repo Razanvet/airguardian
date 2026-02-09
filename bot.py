9eedab0827e312085f3e5cb593e32d8e690047a6
Razanvet <120029950+Razanvet@users.noreply.github.com>
2026-02-09 16:29:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 from telegram import Bot
 
-# ===== НАСТРОЙКИ =====
-BOT_TOKEN = "8552290162:AAGHM0pmC6BuCjE4NlTqG0N3pIGNZ4r4lCc"
-CHAT_ID = "1200659505"
+BOT_TOKEN = "ВАШ_BOT_TOKEN"
+CHAT_ID = ВАШ_CHAT_ID
 
 bot = Bot(token=BOT_TOKEN)
 
-async def send_alert(message: str):
-    print("DEBUG: send_alert called")
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text=message
-    )
-
-
+async def send_or_update_message(text: str, message_id: int | None):
+    if message_id is None:
+        msg = await bot.send_message(
+            chat_id=CHAT_ID,
+            text=text,
+            parse_mode="Markdown"
+        )
+        return msg.message_id
+    else:
+        await bot.edit_message_text(
+            chat_id=CHAT_ID,
+            message_id=message_id,
+            text=text,
+            parse_mode="Markdown"
+        )
+        return message_id
