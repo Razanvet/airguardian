2554ab4efdb3db8741ff9ed5c056941ee1d0fc7f
Razanvet <120029950+Razanvet@users.noreply.github.com>
2026-02-10 19:36:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import sqlite3
 import asyncio
 from aiogram import Bot, types
-from aiogram.exceptions import MessageNotModified
+from aiogram.exceptions import TelegramAPIError
+
 
 
 # ===== Настройки бота =====
@@ -97,4 +98,5 @@ async def main_loop():
 
 if __name__ == "__main__":
     asyncio.run(main_loop())
+
 
